47ad6f505f87e7d084f051ea77b01c14b1ecf18e
muniira-03 <munirra.alsubaie@gmail.com>
2025-04-07 09:41:19 +0000

    new node

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,6 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
-
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -41,5 +41,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+void binary_tree_print(const binary_tree_t *);
 #endif
